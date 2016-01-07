0609c6384ff7db3d0454d53394436e4637fe0bfd
crovelli <crovelli@gmail.com>
2016-01-07 11:29:02 +0100

    extra for jets and leptons

--- analysis/scripts/diPhoAnaBATCH.py
+++ analysis/scripts/diPhoAnaBATCH.py
@@ -15,7 +15,6 @@ process.load("FWCore.MessageService.MessageLogger_cfi")
 
 process.load("Configuration.StandardSequences.GeometryDB_cff")
 process.load("Configuration.StandardSequences.MagneticField_cff")
-#process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff")
 from Configuration.AlCa.GlobalTag import GlobalTag
 
@@ -34,10 +33,9 @@ process.MessageLogger.cerr.FwkReport.reportEvery = cms.untracked.int32( 1000 )
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32( -1 ) )
 
 process.source = cms.Source("PoolSource",
-                            fileNames = cms.untracked.vstring(
+                            fileNames = cms.untracked.vstring('/store/group/phys_higgs/cmshgg/musella/flashgg/ExoPhys14_v2/diphotonsPhys14V2/RSGravToGG_kMpl001_M_5000_Tune4C_13TeV_pythia8/ExoPhys14_v2-diphotonsPhys14V2-v0-Phys14DR-PU20bx25_PHYS14_25_V1-v1/150128_133931/0000/myOutputFile_1.root'
+                           )                                   
 
-#'/store/group/phys_higgs/cmshgg/musella/flashgg/ExoPhys14_v2/diphotonsPhys14V2/RSGravToGG_kMpl001_M_5000_Tune4C_13TeV_pythia8/ExoPhys14_v2-diphotonsPhys14V2-v0-Phys14DR-PU20bx25_PHYS14_25_V1-v1/150128_133931/0000/myOutputFile_1.root'
-                                                              )
 if (isMC==False and is2015DFromChiara):
     print "applying 2015D json"                                
     process.source.lumisToProcess = CfgTypes.untracked(CfgTypes.VLuminosityBlockRange())  
@@ -50,12 +48,25 @@ process.load("flashgg/MicroAOD/flashggDiPhotons_cfi")
 
 process.TFileService = cms.Service("TFileService",fileName = cms.string("OUTPUT"))
 
+# to make jets   
+from flashgg.MicroAOD.flashggJets_cfi import flashggBTag, maxJetCollections
+process.flashggUnpackedJets = cms.EDProducer("FlashggVectorVectorJetUnpacker",
+                                             JetsTag = cms.InputTag("flashggFinalJets"),
+                                             NCollections = cms.uint32(maxJetCollections)
+                                             )
+
+UnpackedJetCollectionVInputTag = cms.VInputTag()
+for i in range(0,maxJetCollections):
+    UnpackedJetCollectionVInputTag.append(cms.InputTag('flashggUnpackedJets',str(i)))                            
+
 process.diPhoAna = cms.EDAnalyzer('NewDiPhoAnalyzer',
                                   VertexTag = cms.untracked.InputTag('offlineSlimmedPrimaryVertices'),
 				  METTag=cms.untracked.InputTag('slimmedMETs'),
+                                  inputTagJets= UnpackedJetCollectionVInputTag,  
+                                  ElectronTag=cms.InputTag('flashggSelectedElectrons'),
+                                  MuonTag=cms.InputTag('flashggSelectedMuons'), 
+                                  bTag = cms.untracked.string(flashggBTag),      
                                   genPhotonExtraTag = cms.InputTag("flashggGenPhotonsExtra"),   
-                                  reducedBarrelRecHitCollection = cms.InputTag('reducedEgamma','reducedEBRecHits'),
-                                  reducedEndcapRecHitCollection = cms.InputTag('reducedEgamma','reducedEERecHits'),
                                   DiPhotonTag = cms.untracked.InputTag('flashggDiPhotons'),
                                   PileUpTag = cms.untracked.InputTag('slimmedAddPileupInfo'),
                                   generatorInfo = cms.InputTag("generator"),
@@ -68,5 +79,5 @@ process.diPhoAna = cms.EDAnalyzer('NewDiPhoAnalyzer',
                                   sumDataset   = SDS
                                   )
 
-process.p = cms.Path(process.diPhoAna)
+process.p = cms.Path(process.flashggUnpackedJets*process.diPhoAna)
 
