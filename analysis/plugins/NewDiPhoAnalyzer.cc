c32b871af53afcf10c29ab98130d2e5fea6225d1
Margaret Zientek <mez34@cornell.edu>
2016-02-01 10:09:10 +0100

    commit before merge (attempt at adding z info)

--- analysis/plugins/NewDiPhoAnalyzer.cc
+++ analysis/plugins/NewDiPhoAnalyzer.cc
@@ -1174,20 +1174,30 @@ void NewDiPhoAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& 
 		genZ = -1;
 		ptZ  = -999.;
 		if (sampleID==11 || sampleID==20){ //VH or ZpZH
-		  std::cout << "sampleID " << sampleID << std::endl;
+		  //std::cout << "sampleID " << sampleID << std::endl;
 		  for (unsigned int genLoop = 0; genLoop < genParticles->size(); genLoop++){
 		    if (genParticles->ptrAt( genLoop )->mother(0)){
 		      int mothid = fabs(genParticles->ptrAt( genLoop )->mother(0)->pdgId()); 
 		      int daugid = fabs(genParticles->ptrAt( genLoop )->pdgId());
 		      std::cout << "mothid " << mothid << " daugid " << daugid << std::endl;
-		      if (mothid==23){ // only interested in Z events
-			if (daugid==12 || daugid==14 || daugid==16){// if Z decays to neutrinos
-			  genZ = 1;// Z->neutrinos
-			  ptZ = genParticles->ptrAt( genLoop )->mother(0)->pt();// for these events store the pt of the Z
+		      if (daugid==23){
+			unsigned int numDaugh = genParticles->ptrAt( genLoop )->mother(0)->numberOfDaughters();
+			std::cout << "number daughters " << numDaugh << std::endl; 
+			bool otherDaughIsHiggs = false;
+			for (unsigned int daugLoop = 0; daugLoop < numDaugh; daugLoop++){
+			  std::cout << "daug ID = " << fabs(genParticles->ptrAt ( genLoop )->mother(0)->daughter( daugLoop )->pdgId()) << std::endl;
+			  if (fabs(genParticles->ptrAt ( genLoop )->mother(0)->daughter( daugLoop )->pdgId()) == 25) otherDaughIsHiggs = true;  
 			}
-		        else genZ = 2;// Z NOT -> neutrinos
+			std::cout << "otherDaughIsHiggs " << otherDaughIsHiggs<< std::endl;
+		      } 
+		      //std::cout << "daug1id " << daug1id << " daug2id " << daug2id << std::endl;
+		      if (mothid==23){ // only interested in Z events
+		        genZ = 1;// Z event
+		        if (daugid==12 || daugid==14 || daugid==16){// if Z decays to neutrinos
+
+		          ptZ = genParticles->ptrAt( genLoop )->mother(0)->pt();// for these events store the pt of the Z
+		        }
 		      }
-		      else genZ = 0;// NO Z in event
 		    }
 		  }
 		}
